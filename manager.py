d5af53aee89db44400190f2096490b25212acb00
Tyler Fink <tsfink182@stevenscollege.edu>
2019-02-10 18:13:48 -0500

    Was able to get my runner to work. I thought i had my if else inside my createNewTask function but it doesnt work. It was actually overwritting everything in the file which was pretty weird

--- manager.py
+++ manager.py
@@ -1,38 +1,47 @@
+from item import Item
 import time
 import datetime
 import item
-import dedent
-
+from textwrap import dedent
 
 class Manager(object):
+
+    def __init__(self):
+        self.todos = []
+        file_obj = open('todos.txt', 'r')
+        self.todos = file_obj.readlines()
+
+
     def showItems(self):
         # variable to hold all the content inside of the todos.txt
         readItems = open("todos.txt", "r")# read mode
-        # creating an if statement so that you can choose what you want to do
+        # creating an if tatement so that you can choose what you want to do
             # setting the contents inside the variable readitems to the message
         message = readItems.read()
         print(message)
         readItems.close()
 
-    def CreateNewTask(self):
-        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        choice = input('> ')
-        
-        of = open("todos.txt","a+")#append mode
+    def CreateNewTask(self, amount):
+        for task in amount:
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            of = open("todos.txt","a+")#append mode
         # creating an if statement so that you can choose what you want to do
-        task = input("Create A New Task: ")
-        of.write("\n" + now + " " + task)
-        of.close()
-
+            task = input("Create A New Task: ")
+            of.write("\n" + now + " " + task)
+            of.close()
 
     def markComplete(self):
+        printTask = open("todos.txt", "r")
+        message = printTask.read()
+        print(message)
+        printTask.close()
         # Creating variable to hold the contents in the file
         editTask = open("todos.txt").read()
-        taskCompleted = input("Have you finished any tasks?")
+        taskCompleted = input("Did you complete anything today? I hope so! ")
         # new variable to hold the new contents
         markTask = editTask.replace(taskCompleted, taskCompleted + " " + str(True))
         # opening the file to so i can write in the new tasks
-        newMarkedTask = open("todos.txt", "w+")
+        newMarkedTask = open("todos.txt", "w")
         # writing the replaced data to the new instance of the file
         newMarkedTask.write(markTask)
         # always close open
@@ -43,11 +52,13 @@ class Manager(object):
         print(message)
         printTask.close()
 
-    def runner(object):
+
+
+    def run(self):
         print('What is your name?')
         name = input('> ')
+        print(f'{name}, Welcome to your todo manager!')
         while True:
-            print(f'{name}, Welcome to your todo manager!')
             choice = input('> ')
             if choice == 'add' or choice == 'a':
                 self.CreateNewTask()
@@ -59,32 +70,6 @@ class Manager(object):
                 exit(0)
             elif choice == 'help':
                 print(dedent('''
-                            You may use "add" to update your list with  another task; "list" to show your complete  list; "mark" will ask what task you have    completed; "quit" will exit program.''')
-
-
-
-
-
-
-
-
-
-
-# class Manager(Item):
-#
-#     def add_item(text):
-#         newitem = Item.lists.append(text)
-#
-#
-#     def completed(self):
-#         if list[1] + 2 == False:
-#             Print("Have you done this today?")
-#             choice = input('> ')
-#             if choice == 'yes':
-#                 list[1] = True
-#
-#             if choice == 'no':
-#                 list[1] == False
-#
-# add = Manager()
-# add.add_tem('Hello')
+                            You may use "add" to update your list with  another task; "list" to show your complete  list; "mark" will ask what task you have completed; "quit" will exit program.'''))
+            else:
+                print('You can use "help" to see a list ')
