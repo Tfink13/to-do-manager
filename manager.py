c3e5b8d663957ed52f4e0b84b5c9fb4333d60008
Tyler Fink <tsfink182@stevenscollege.edu>
2019-02-10 18:39:44 -0500

    Added a prompt asking how many entries youd like to enter, also a limit just in case someone puts alot and gets "stuck"

--- manager.py
+++ manager.py
@@ -1,4 +1,3 @@
-from item import Item
 import time
 import datetime
 import item
@@ -6,38 +5,34 @@ from textwrap import dedent
 
 class Manager(object):
 
-    def __init__(self):
-        self.todos = []
-        file_obj = open('todos.txt', 'r')
-        self.todos = file_obj.readlines()
-
-
     def showItems(self):
         # variable to hold all the content inside of the todos.txt
         readItems = open("todos.txt", "r")# read mode
         # creating an if tatement so that you can choose what you want to do
-            # setting the contents inside the variable readitems to the message
+        # setting the contents inside the variable readitems to the message
         message = readItems.read()
         print(message)
         readItems.close()
 
     def CreateNewTask(self, amount):
-        for task in amount:
-            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        for amount in range(20):
+            now = datetime.datetime.now().strftime("%Y-%m-%d%H:%M:%S")
             of = open("todos.txt","a+")#append mode
-        # creating an if statement so that you can choose what you want to do
+            # creating an if statement so that you can choose what you want to do
             task = input("Create A New Task: ")
-            of.write("\n" + now + " " + task)
+            of.write("\n" + now + " " + task + str(False))
             of.close()
 
     def markComplete(self):
+        # I wanted to print the list so you are able to see what is int he list. Neither True or False means it was not completed
         printTask = open("todos.txt", "r")
         message = printTask.read()
         print(message)
         printTask.close()
+
         # Creating variable to hold the contents in the file
         editTask = open("todos.txt").read()
-        taskCompleted = input("Did you complete anything today? I hope so! ")
+        taskCompleted = input("Want to mark anything as completed?")
         # new variable to hold the new contents
         markTask = editTask.replace(taskCompleted, taskCompleted + " " + str(True))
         # opening the file to so i can write in the new tasks
@@ -46,6 +41,7 @@ class Manager(object):
         newMarkedTask.write(markTask)
         # always close open
         newMarkedTask.close()
+
 
         printTask = open("todos.txt", "r")
         message = printTask.read()
@@ -61,7 +57,12 @@ class Manager(object):
         while True:
             choice = input('> ')
             if choice == 'add' or choice == 'a':
-                self.CreateNewTask()
+                print('How many entries?')
+                amount = input('> ')
+                if amount >= '20':
+                    print('You may not have that many entries.')
+                else:
+                    self.CreateNewTask(amount)
             elif choice == 'list' or choice == 'l':
                 self.showItems()
             elif choice == 'mark' or choice == 'm':
@@ -72,4 +73,4 @@ class Manager(object):
                 print(dedent('''
                             You may use "add" to update your list with  another task; "list" to show your complete  list; "mark" will ask what task you have completed; "quit" will exit program.'''))
             else:
-                print('You can use "help" to see a list ')
+                print('You can use "help" to see a list of commands')
